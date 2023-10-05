7ab9e3376b38531e5224375692cfe8ed359b4094
nishu <83027353+nishusapkota@users.noreply.github.com>
2023-10-05 14:13:54 +0545

    integrating admin template

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,16 +1,13 @@
-<x-app-layout>
-    
-</x-app-layout>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin Dashboard</title>
-</head>
-<body>
-    <h1>This is admin dashboard</h1>
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+@include('admin.header')
+        <!-- partial -->
+        @include('admin.body')
+    <!-- container-scroller -->
+    @include('admin.script')   
 </body>
 </html>
